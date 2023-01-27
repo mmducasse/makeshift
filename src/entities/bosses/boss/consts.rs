a756fc1832b5d471f43a6b72d33b7846bc921e0b
Matthew Maurice Ducasse <matthewmducasse@gmail.com>
2023-01-26 19:35:49 -0800

    Made boss speeds all integers.

--- src/entities/bosses/boss/consts.rs
+++ src/entities/bosses/boss/consts.rs
@@ -1,8 +1,8 @@
 
-pub const RUN_SPEED_X: f32 = 1.2;
+pub const RUN_SPEED_X: f32 = 1.0;
 pub const DASH_SPEED_X: f32 = 4.0;
 pub const FLY_SPEED_SLOW: f32 = 1.0;
-pub const FLY_SPEED_FAST: f32 = 2.5;
+pub const FLY_SPEED_FAST: f32 = 3.0;
 pub const JUMP_VEL_Y: f32 = -5.0;
 pub const JUMP_RELEASE_VEL_Y: f32 = -2.0;
 pub const MAX_FALL_VEL_Y: f32 = 5.0;
